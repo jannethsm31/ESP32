9a20b7d7b765b595e3e67cd67452a482cf14908b
jannethsm31 <1722110301@utectulancingo.edu.mx>
2023-11-29 22:55:20 -0600

    Actualizar

--- sql/dispositivos.sql
+++ sql/dispositivos.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS dispositivos;
 CREATE TABLE dispositivos(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     dispositivo VARCHAR(10) NOT NULL,
-    valor tinyint NOT NULL DEFAULT 0
+    valor INTEGER NOT NULL DEFAULT 0
 );
 
 INSERT INTO dispositivos(dispositivo, valor) VALUES ('led', 0), ('potenciometro', 0);
