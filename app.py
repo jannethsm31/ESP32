95ad8da473237f306cb17089f9e185eb030d3edd
jannethsm31 <1722110301@utectulancingo.edu.mx>
2023-11-30 20:58:24 -0600

    front

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,11 +6,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/ver", methods=["GET", "POST"])
-def ver():
-    return render_template('ver.html')
 
-@app.route("/observar", methods=["GET"])
-def observar():
-    """Pagina para probar DOM"""
-    return render_template('observar.html')
+if __name__ == "__main__":
+    app.run(debug=True)
